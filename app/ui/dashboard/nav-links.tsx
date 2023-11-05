d25ff5fb2f597bf7ad897fe2240245d65dc9108e
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-05 21:49:25 +0800

    feat: add chapter 5 - refactor to client component for detecting active links

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import {
   DocumentDuplicateIcon,
   HomeIcon,
   UserGroupIcon,
 } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -18,6 +22,8 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+
   return (
     <>
       {links.map((link) => {
@@ -26,7 +32,12 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex gap-2 justify-center items-center p-3 text-sm font-medium bg-gray-50 rounded-md md:flex-none md:justify-start md:p-2 md:px-3 hover:text-blue-600 h-[48px] grow hover:bg-sky-100"
+            className={clsx(
+              "flex gap-2 justify-center items-center p-3 text-sm font-medium bg-gray-50 rounded-md md:flex-none md:justify-start md:p-2 md:px-3 hover:text-blue-600 h-[48px] grow hover:bg-sky-100",
+              {
+                "bg-sky-100 text-blue-600": pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
