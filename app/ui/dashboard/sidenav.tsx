8ccb37d23238f2b088d68c50218b9fe384ce3373
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-05 11:34:17 +0800

    feat: add chapter 4 - dashboard layout and pages

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -1,24 +1,27 @@
-import Link from 'next/link';
-import NavLinks from '@/app/ui/dashboard/nav-links';
-import AcmeLogo from '@/app/ui/acme-logo';
-import { PowerIcon } from '@heroicons/react/24/outline';
+import AcmeLogo from "@/app/ui/acme-logo";
+import NavLinks from "@/app/ui/dashboard/nav-links";
+import { PowerIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
 
 export default function SideNav() {
   return (
-    <div className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="flex flex-col py-4 px-3 h-full md:px-2">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
+        className="flex justify-start items-end p-4 mb-2 h-20 bg-blue-600 rounded-md md:h-40"
         href="/"
       >
         <div className="w-32 text-white md:w-40">
           <AcmeLogo />
         </div>
       </Link>
-      <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+      <div className="flex flex-row justify-between space-x-2 md:flex-col md:space-y-2 md:space-x-0 grow">
         <NavLinks />
-        <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+        <div className="hidden w-full h-auto bg-gray-50 rounded-md md:block grow" />
         <form>
-          <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
+          <button
+            type="button"
+            className="flex gap-2 justify-center items-center p-3 text-sm font-medium bg-gray-50 rounded-md md:flex-none md:justify-start md:p-2 md:px-3 hover:text-blue-600 h-[48px] grow hover:bg-sky-100"
+          >
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
           </button>
