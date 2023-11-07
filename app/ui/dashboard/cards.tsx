265673007dbef4903efd58e8651467d038b5a725
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-07 23:40:39 +0800

    feat: add chapter 7 - data fetching with server component

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -1,10 +1,11 @@
 import {
   BanknotesIcon,
   ClockIcon,
-  UserGroupIcon,
   InboxIcon,
-} from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+  UserGroupIcon,
+} from "@heroicons/react/24/outline";
+
+import { lusitana } from "@/app/ui/fonts";
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -37,14 +38,14 @@ export function Card({
 }: {
   title: string;
   value: number | string;
-  type: 'invoices' | 'customers' | 'pending' | 'collected';
+  type: "invoices" | "customers" | "pending" | "collected";
 }) {
   const Icon = iconMap[type];
 
   return (
-    <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
+    <div className="p-2 bg-gray-50 rounded-xl shadow-sm">
       <div className="flex p-4">
-        {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
+        {Icon ? <Icon className="w-5 h-5 text-gray-700" /> : null}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,30 +1,31 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
+import { ArrowPathIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
+
+import Image from "next/image";
+
+import { LatestInvoice } from "@/app/lib/definitions";
+import { lusitana } from "@/app/ui/fonts";
+
 export default async function LatestInvoices({
   latestInvoices,
 }: {
   latestInvoices: LatestInvoice[];
 }) {
   return (
-    <div className="flex w-full flex-col md:col-span-4 lg:col-span-4">
+    <div className="flex flex-col w-full md:col-span-4 lg:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
-      <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
-        {/* NOTE: comment in this code when you get to this point in the course */}
-
-        {/* <div className="bg-white px-6">
+      <div className="flex flex-col justify-between p-4 bg-gray-50 rounded-xl grow">
+        <div className="px-6 bg-white">
           {latestInvoices.map((invoice, i) => {
             return (
               <div
                 key={invoice.id}
                 className={clsx(
-                  'flex flex-row items-center justify-between py-4',
+                  "flex flex-row items-center justify-between py-4",
                   {
-                    'border-t': i !== 0,
+                    "border-t": i !== 0,
                   },
                 )}
               >
@@ -37,7 +38,7 @@ export default async function LatestInvoices({
                     height={32}
                   />
                   <div className="min-w-0">
-                    <p className="truncate text-sm font-semibold md:text-base">
+                    <p className="text-sm font-semibold md:text-base truncate">
                       {invoice.name}
                     </p>
                     <p className="hidden text-sm text-gray-500 sm:block">
@@ -53,10 +54,10 @@ export default async function LatestInvoices({
               </div>
             );
           })}
-        </div> */}
-        <div className="flex items-center pb-2 pt-6">
-          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
-          <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
+        </div>
+        <div className="flex items-center pt-6 pb-2">
+          <ArrowPathIcon className="w-5 h-5 text-gray-500" />
+          <h3 className="ml-2 text-sm text-gray-500">Updated just now</h3>
         </div>
       </div>
     </div>
