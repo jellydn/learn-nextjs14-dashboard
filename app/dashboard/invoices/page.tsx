8982d53e38885b20bdc72da0174314dde7c8425b
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-12 14:01:55 +0800

    feat: add chapter 11 - invoice page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,38 @@
-export default function Page() {
-  return <p>Invoices Page</p>;
+import { Suspense } from "react";
+
+import { lusitana } from "@/app/ui/fonts";
+import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import Pagination from "@/app/ui/invoices/pagination";
+import InvoicesTable from "@/app/ui/invoices/table";
+import Search from "@/app/ui/search";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  return (
+    <div className="w-full">
+      <div className="flex justify-between items-center w-full">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="flex gap-2 justify-between items-center mt-4 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <InvoicesTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="flex justify-center mt-5 w-full">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
 }
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,7 +2,25 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+import logger from "../lib/logger";
+
 export default function Search({ placeholder }: { placeholder: string }) {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(keyword: string) {
+    logger.info("Searching for", keyword);
+    const params = new URLSearchParams(searchParams);
+    if (keyword) {
+      params.set("query", keyword);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
   return (
     <div className="flex relative flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -11,6 +29,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="block pl-10 w-full text-sm rounded-md border border-gray-200 peer py-[9px] outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
+        onChange={(e) => handleSearch(e.target.value)}
+        defaultValue={searchParams.get("query")?.toString() ?? ""}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 text-gray-500 -translate-y-1/2 h-[18px] w-[18px] peer-focus:text-gray-900" />
     </div>
