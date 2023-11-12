d4ddc90b859dd445e3a05c75ec97477f133a744c
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-12 16:56:46 +0800

    feat: add chapter 13 - add 404 page for not found invoice

---
+++ app/dashboard/invoices/[id]/edit/not-found.tsx
@@ -0,0 +1,19 @@
+import { FaceFrownIcon } from "@heroicons/react/24/outline";
+
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="flex flex-col gap-2 justify-center items-center h-full">
+      <FaceFrownIcon className="w-10 text-gray-400" />
+      <h2 className="text-xl font-semibold">404 Not Found</h2>
+      <p>Could not find the requested invoice.</p>
+      <Link
+        href="/dashboard/invoices"
+        className="py-2 px-4 mt-4 text-sm text-white bg-blue-500 rounded-md transition-colors hover:bg-blue-400"
+      >
+        Go Back
+      </Link>
+    </main>
+  );
+}
