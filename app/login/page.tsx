53fafb487b6977d8a986155948352b3fbf2ea786
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-13 00:35:04 +0800

    fix: remove anchor link on logo

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,7 @@
 import { type Metadata } from "next";
 
+import Link from "next/link";
+
 import AcmeLogo from "@/app/ui/acme-logo";
 import LoginForm from "@/app/ui/login-form";
 
@@ -13,7 +15,9 @@ export default function LoginPage() {
       <div className="flex relative flex-col p-4 mx-auto space-y-2.5 w-full md:-mt-32 max-w-[400px]">
         <div className="flex items-end p-3 w-full h-20 bg-blue-500 rounded-lg md:h-36">
           <div className="w-32 text-white md:w-36">
-            <AcmeLogo />
+            <Link href="/">
+              <AcmeLogo />
+            </Link>
           </div>
         </div>
         <LoginForm />
