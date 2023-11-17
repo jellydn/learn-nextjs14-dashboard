a1ce8279a7ef5598806e66a00b36f1d55dc8f211
Huynh Duc Dung <dunghd.it@gmail.com>
2023-11-17 22:59:26 +0800

    ci: fix load image on Vercel

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["nextjs14.productsway.com"],
+  },
+};
 
 module.exports = nextConfig;
